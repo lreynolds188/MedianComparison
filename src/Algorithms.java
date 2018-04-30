41f0e1e746c892c954238b4210242a5bae17b76b
Luke Reynolds <lreynolds188@gmail.com>
2018-04-30 19:59:21 +1000

    - added brute force algorithm functionality

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,7 +1,27 @@
 public class Algorithms {
 
+    /**
+     * Returns the median value in a given array A of n numbers.
+     * @param array
+     * @return
+     */
     public static int BruteForceMedian(int[] array){
-        return 0;
+        int k = (int)Math.ceil((array.length+1)/2);
+        for (int i = 0; i < array.length; i++){
+            int smaller = 0;
+            int equal = 0;
+            for (int j = 0; j < array.length; j++){
+                if (array[j] < array[i]){
+                    smaller += 1;
+                } else if (array[j] == array[i]){
+                    equal += 1;
+                }
+            }
+            if (smaller < k && k <= (smaller + equal)){
+                return array[i];
+            }
+        }
+        return k;
     }
 
     public static int Median(int[] array){
