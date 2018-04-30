2225c8eb06fa901c4c0d09faf065292ff849ef6c
Luke Reynolds <lreynolds188@gmail.com>
2018-04-30 16:59:09 +1000

    - modified functions to be static

---
+++ src/Algorithms.java
@@ -0,0 +1,19 @@
+public class Algorithms {
+
+    public static int BruteForceMedian(int[] array){
+        return 0;
+    }
+
+    public static int Median(int[] array){
+        return 0;
+    }
+
+    public static int Select(int[] array, int low, int mid, int high){
+        return 0;
+    }
+
+    public static int Partition(int[] array, int low, int high){
+        return 0;
+    }
+
+}
