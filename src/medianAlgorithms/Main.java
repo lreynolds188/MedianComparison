1ef1701c953ccb9f55ecf3d9e774b37f37d53bed
Kathryn Dorge <kathryn.dorge@connect.qut.edu.au>
2018-05-07 14:04:20 +1000

    - moved generate random arrays to a seperate method within main

--- src/medianAlgorithms/Main.java
+++ src/medianAlgorithms/Main.java
@@ -5,6 +5,22 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Main{
+	
+	
+	public static int[] generateRandomArray( int size )
+	{
+		Random randomNumberGenerator = new Random();
+		
+		int[] randArray = new int[size];
+		
+		for ( int jj = 0; jj < size; jj++ )
+		{
+			randArray[jj] = randomNumberGenerator.nextInt();
+		}
+		
+		return randArray;
+	}
+	
 
     public static void main(String[] args) throws IOException
     {
@@ -14,17 +30,12 @@ public class Main{
     	
     	int[] sizeOfArray = new int[numArraysTested];
     	long[] basicOperationCounter = new long[numArraysTested];
-    	
-    	Random randomNumberGenerator = new Random();
+
     	
     	FileWriter fl = new FileWriter( "basicOperationsTest.csv" ); 
     	
     	for ( int j = 0; j < numArraysTested; j++ )
     		basicOperationCounter[j] = 0;
-    	
-    	
-    	
-    	
     	
     	for ( int i = 0; i < numTests; i++ )
     	{
@@ -33,12 +44,7 @@ public class Main{
     		{
     			System.out.println( "starting array size test " + (j + 1) );
     			
-    			int[] randArray = new int[size];
-    			
-    			for ( int jj = 0; jj < size; jj++ )
-    			{
-    				randArray[jj] = randomNumberGenerator.nextInt();
-    			}
+    			int[] randArray = generateRandomArray( size );
     			
     			
     			PartitionAlgorithm.basicCounter = 0;
