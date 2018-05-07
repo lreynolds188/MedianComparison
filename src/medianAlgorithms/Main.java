396a051b68a0524ef808e36f48311ffd020522d4
Kathryn Dorge <kathryn.dorge@connect.qut.edu.au>
2018-05-07 13:56:27 +1000

    - seperated partitioning algorithm to seperate class file - added basic operations counter method and static variable for partitioning method - updated test cases for partitioning median algorithm - Main method added with frame work to test for partitioning basic ops - Main method writes the collect data to csv file format - MATLAB file (.m) is used to graph the results to view them

--- src/medianAlgorithms/Main.java
+++ src/medianAlgorithms/Main.java
@@ -1,8 +1,75 @@
 package medianAlgorithms;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class Main{
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException
+    {
+    	int numTests = 300;
+    	int size = 1000;
+    	int numArraysTested = 50;
+    	
+    	int[] sizeOfArray = new int[numArraysTested];
+    	long[] basicOperationCounter = new long[numArraysTested];
+    	
+    	Random randomNumberGenerator = new Random();
+    	
+    	FileWriter fl = new FileWriter( "basicOperationsTest.csv" ); 
+    	
+    	for ( int j = 0; j < numArraysTested; j++ )
+    		basicOperationCounter[j] = 0;
+    	
+    	
+    	
+    	
+    	
+    	for ( int i = 0; i < numTests; i++ )
+    	{
+    		System.out.println( "Starting test " + (i + 1) + "\n\n\n");
+    		for ( int j = 0; j < numArraysTested; j++ )
+    		{
+    			System.out.println( "starting array size test " + (j + 1) );
+    			
+    			int[] randArray = new int[size];
+    			
+    			for ( int jj = 0; jj < size; jj++ )
+    			{
+    				randArray[jj] = randomNumberGenerator.nextInt();
+    			}
+    			
+    			
+    			PartitionAlgorithm.basicCounter = 0;
+    			
+    			PartitionAlgorithm.MedianBasicOperationCount( randArray );
+    			
+    			
+    			// save data
+    			sizeOfArray[j] = size;
+    			basicOperationCounter[j] += PartitionAlgorithm.basicCounter;
+    			
+    			System.out.println( "Basic operations performed: " + PartitionAlgorithm.basicCounter + "\n");
+    			
+    			// incrament size of array
+    			size += 10000;
+    		}
+    	}
+    	
+    	// calculate average
+    	for ( int j = 0; j < numArraysTested; j++ )
+    	{
+    		basicOperationCounter[j] /= numTests;
+    		
+    		// save data in csv format
+    		fl.write( sizeOfArray[j] + "," );
+    		fl.write( basicOperationCounter[j] + "\n");
+    	}
+    	
+    	
+    	
+    	
+    	fl.close();
     }
 }
--- src/testCases/TestPartitionMedian.java
+++ src/testCases/TestPartitionMedian.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import medianAlgorithms.Algorithms;
 import medianAlgorithms.PartitionAlgorithm;
 
 public class TestPartitionMedian {
@@ -14,9 +13,9 @@ public class TestPartitionMedian {
 	{
 		assertEquals( 5, PartitionAlgorithm.Median( new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } ) );
 		assertEquals( 4, PartitionAlgorithm.Median( new int[] { 9, 1, 2, 8, 3, 4, 4, 1, 9, 2 } ) );
-		assertEquals( 5, PartitionAlgorithm.Median( new int[] { 1, 2, 3, 4, 6, 6, 7, 8, 5 } ) );
-		assertEquals( 7, PartitionAlgorithm.Median( new int[] { 5, 9, 11, 0, -4, 6, 7, 60, 11, 99, 1 } ) );
-		assertEquals( 7, PartitionAlgorithm.Median( new int[] { 5, 9, 11, 0, -4, 6, 7, 60, 11, 99, 1, -99 } ) );
+		assertEquals( 7, PartitionAlgorithm.Median( new int[] { -5, 9, 11, -1, -4, 6, 7, 60, 11, 99, 1 } ) );
+		assertEquals( 7, PartitionAlgorithm.Median( new int[] { -5, 9, 11, -1, -4, 6, 7, 60, 11, 99, 1, -99 } ) );
+		assertEquals( 4, PartitionAlgorithm.Median( new int[] { 0, 1, 2, 3, 80, 7, 90, 4 } ) );
 	}
 
 	
